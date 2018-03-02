58edc5f28d9c37e09b2a01f7e0e3cc6d7a5a33dc
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-03-02 20:43:53 +0900

    演習1-1の成果物

---
+++ DDT-work/src/java_1/E01_01.java
@@ -0,0 +1,32 @@
+package java_1;
+
+/*
+ * 演習1-1
+ * プログラム中の文の終端を示すセミコロン；が欠如しているとどうなるか。
+ * プログラムをコンパイルして検証せよ。
+ *
+ *
+ */
+
+public class E01_01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+
+		System.out.println("初めてのJavaプログラム。");
+		System.out.println("画面に出力しています。")
+
+		/*
+		 * Javaの文については基本的に末尾に;をつける必要があるため
+		 */
+
+
+
+
+
+
+	}
+
+}
