ee2396bd1c6dd521bebc26baf60e0761cc246700
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-03-15 00:28:20 +0900

    演習3-18をコミット(季節を変数に代入する処理がそれぞれ1回になるよう修正）

--- DDT-work/src/java03/E03_18.java
+++ DDT-work/src/java03/E03_18.java
@@ -8,89 +8,60 @@ import java.util.Scanner;
 public class E03_18 {
 
 	public static void main(String[] args) {
-		//標準入力ストリーム
+		// 標準入力ストリーム
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("月(1～12の整数)を入力してください＞");
-		//月を入力(１～１２の整数）
+		// 月を入力(１～１２の整数）
 		int inputMonth = scanner.nextInt();
 
-		String inputSeason = null; //季節を格納する変数
-
-
-
+		String inputSeason = null; // 季節を格納する変数
 
 		switch (inputMonth) {
+		case 12:
+
 		case 1:
-			//入力値が1なら「冬」
-			inputSeason = "冬";
-			break;
 
 		case 2:
-			//入力値が2なら「冬」
+			// 入力値が12、1、2なら「冬」
 			inputSeason = "冬";
 			break;
 
 		case 3:
-			//入力値が3なら「春」
-			inputSeason = "春";
-			break;
+
 		case 4:
-			//入力値が4なら「春」
-			inputSeason = "春";
-			break;
 
 		case 5:
-			//入力値が5なら「春」
+			// 入力値が3～5なら「春」
 			inputSeason = "春";
 			break;
 
-
 		case 6:
-			//入力値が6なら「夏」
-			inputSeason = "夏";
-			break;
 
 		case 7:
-			//入力値が7なら「夏」
-			inputSeason = "夏";
-			break;
 
 		case 8:
-			//入力値が8なら「夏」
+			// 入力値が6～8なら「夏」
 			inputSeason = "夏";
 			break;
 
 		case 9:
-			//入力値が9なら「秋」
-			inputSeason = "秋";
-			break;
 
 		case 10:
-			//入力値が10なら「秋」
-			inputSeason = "秋";
-			break;
 
 		case 11:
-			//入力値が11なら「秋」
+			// 入力値が9～11なら「秋」
 			inputSeason = "秋";
 			break;
 
-		case 12:
-			//入力値が12なら「冬」
-			inputSeason = "冬";
-			break;
-
-
 		default:
-			//1～12以外の整数を読み込んだ時のメッセージ
+			// 1～12以外の整数を読み込んだ時のメッセージ
 			System.out.println("対象外の値が入力されました。");
 			break;
 		}
 
-		//入力値に対する季節を出力
+		// 入力値に対する季節を出力
 		System.out.println(inputMonth + "月は" + inputSeason + "です。");
-
 
 	}
 
