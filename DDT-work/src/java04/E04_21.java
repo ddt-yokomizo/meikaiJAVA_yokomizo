815fbc0d90cd44f69c99e7fd65e2d68e59d353f6
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-03-29 10:04:43 +0900

    演習4−21をコミット（コメントと、tmpNumの初期化の箇所を訂正）

--- DDT-work/src/java04/E04_21.java
+++ DDT-work/src/java04/E04_21.java
@@ -15,26 +15,26 @@ public class E04_21 {
 		Scanner scanner = new Scanner(System.in);
 		// 作業用変数
 		int tmpNum = 0;
+		// 入力値を宣言
+		int inputNum1 = 0;
 
 		try {
 			System.out.print("整数値＞");
 			// 入力値１
-			int inputNum1 = scanner.nextInt();
+			inputNum1 = scanner.nextInt();
 
 			/*******************************
 			 * 左上が直角のパターン
 			 ******************************/
 			System.out.println("左上が直角の三角形を表示します。");
-			// 作業変数を０とする
-			tmpNum = 0;
-			// 入力回数分ループ（段の作成）
+			// 段数分ループ（段の作成）
 			for (int i = 0; i < inputNum1; i++) {
 				// 行の作成（処理を行うたびに＊を横に並べる回数が減る）
 				for (int j = 0; j < (inputNum1 - tmpNum); j++) {
 					// 入力値分、＊を横に並べる
 					System.out.print("＊");
 				}
-				// 次の集会で列を減らすために作業用変数をインクリメント
+				// 次のループの空白スペースを確保
 				tmpNum++;
 				// １行分全て終了したタイミングで改行
 				System.out.println("");
@@ -46,7 +46,7 @@ public class E04_21 {
 			System.out.println("右下が直角の三角形を表示します。");
 			// 作業変数を入力値−１に更新する（最初の１段目が1幅あるため）
 			tmpNum = inputNum1 - 1;
-			// 入力回数分ループ（段の作成）
+			// 段数分ループ（段の作成）
 			for (int i = 0; i < inputNum1; i++) {
 				// 行の作成(空白スペースの確保）
 				for (int j = 0; j < tmpNum; j++) {
@@ -57,7 +57,7 @@ public class E04_21 {
 					// 列のスペースで余った部分に、＊を横に並べる
 					System.out.print("＊");
 				}
-				// 次の周回で空白減らすために作業用変数をデクリメント
+				// 次のループの空白スペースを確保
 				tmpNum--;
 				// １行分全て終了したタイミングで改行
 				System.out.println("");
@@ -69,7 +69,7 @@ public class E04_21 {
 			System.out.println("右上が直角の三角形を表示します。");
 			// 作業変数を0にリセットする（最初の１段目が1幅あるため）
 			tmpNum = 0;
-			// 入力回数分ループ（段の作成）
+			// 段数分ループ（段の作成）
 			for (int i = 0; i < inputNum1; i++) {
 				// 行の作成(空白スペースの確保）
 				for (int j = 0; j < tmpNum; j++) {
@@ -80,7 +80,7 @@ public class E04_21 {
 					// 列のスペースで余った部分に、＊を横に並べる
 					System.out.print("＊");
 				}
-				// 次の周回で空白を増やすために作業用変数をインクリメント
+				// 次のループので空白スペースを確保
 				tmpNum++;
 				// １行分全て終了したタイミングで改行
 				System.out.println("");
