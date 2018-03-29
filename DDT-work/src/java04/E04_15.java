de060f8c1d4ca46c7579ee8c3ea808dcf000f736
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-03-29 09:56:30 +0900

    演習4−15をコミット（変数を冒頭で宣言するように修正）

--- DDT-work/src/java04/E04_15.java
+++ DDT-work/src/java04/E04_15.java
@@ -16,37 +16,37 @@ public class E04_15 {
 	public static void main(String[] args) {
 		// 標準入力ストリーム
 		Scanner scanner = new Scanner(System.in);
-		//係数を宣言
+		// 係数を宣言
 		Double coefficientNum = 0.9;
-		//標準体重を宣言
+		// 標準体重を宣言
 		Double standardWeight = 0.0;
 
 		System.out.print("何㎝から：");
 		// 開始値を入力
 		int startHeight = scanner.nextInt();
-		//作業用の変数(初期値を開始値とする）
+		// 作業用の変数(初期値を開始値とする）
 		int tmpNum = startHeight;
-		
+
 		System.out.print("何㎝まで：");
 		// 終了値を入力
 		int endHeight = scanner.nextInt();
-		
+
 		System.out.print("何㎝ごと：");
 		// 増分を入力
 		int increaseValue = scanner.nextInt();
-		
+
 		System.out.println("身長　・　標準体重");
-		
-		//【処理概要】入力値に応じて必要回数分ループし、身長　・　標準体重を出力する。
-		for(int i = 0; i < ((endHeight - startHeight) / increaseValue) + 1 ; i++){
-			//標準体重を計算
-			standardWeight = (tmpNum - 100) * coefficientNum; 
-			//身長と、対応する標準体重を出力
+
+		// 【処理概要】入力値に応じて必要回数分ループし、身長 ・ 標準体重を出力する。
+		for (int i = 0; i < ((endHeight - startHeight) / increaseValue) + 1; i++) {
+			// 標準体重を計算
+			standardWeight = (tmpNum - 100) * coefficientNum;
+			// 身長と、対応する標準体重を出力
 			System.out.println("身長" + tmpNum + "㎝の標準体重は" + standardWeight + "Kgです。");
-			
-			//増分を加算
+
+			// 増分を加算
 			tmpNum += increaseValue;
-			
+
 		}
 
 	}
