91dc3a9f3860128180ed0a220a091704be968b05
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-04-13 19:14:41 +0900

    演習７−５をコミット（新規）

---
+++ DDT-work/src/java07/E07_05.java
@@ -0,0 +1,18 @@
+package java07;
+/**
+ * 「こんにちは」と表示して改行するメソッドを作成せよ。
+ */
+
+public class E07_05 {
+	// 「こんにちは」と表示して改行するメソッド
+	static void hello() {
+		//「こんにちは」と表示して改行
+		System.out.println("こんにちは\n");
+	}
+
+	public static void main(String[] args) {
+		//helloメソッドを呼び出す
+		hello();
+		
+	}
+}
