3bf39d5ebc81301d6d871b233beee92dec924876
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-07-31 21:29:45 +0900

    WIP演習１３−３（定数クラス）をコミット（ジャンケン用の定数を追加）

--- DDT-work/java13/number/Constants.java
+++ DDT-work/java13/number/Constants.java
@@ -16,4 +16,13 @@ public class Constants {
 	public final static int sLESS = 0; //「〜以下」を表すクラス定数
 	public final static int sZERO = 0; //定数0
 	public final static int sONE = 1; //定数1
+	
+	/* ====ジャンケン関連==== */
+	public final static String[] sHANDS = {"グー","チョキ","パー"}; //ジャンケンの手(定数)
+	public final static String sINPUT_MSG = "ジャンケンの手を入力（０・・グー/ １・・チョキ / ２・・パー ）"; //ジャンケン入力時のメッセージ
+	public final static int sWIN = 2; //勝利の定数
+	public final static int sLOSE = 1; //負けの定数
+	public final static int sDRAW = 0; //引き分けの定数
+	public final static int sJK_MIN = 0; //ジャンケンで出せる最小値
+	public final static int sJK_MAX = 2; //ジャンケンで出せる最大値
 }
