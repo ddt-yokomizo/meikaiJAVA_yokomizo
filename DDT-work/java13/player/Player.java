a95f023e29ef245db986f2949920661300dd9fc3
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-07-31 21:31:29 +0900

    WIP演習１３−３（プレイヤー抽象クラス）をコミット（未検証であるためWIP）

--- DDT-work/java13/player/Player.java
+++ DDT-work/java13/player/Player.java
@@ -1,4 +1,6 @@
 package player;
+import static number.Constants.*;
+
 /* ====================================================================== */
 /**
  * @brief ジャンケンのプレイヤーを表す抽象クラス
@@ -9,8 +11,8 @@ package player;
 /* ====================================================================== */
 public abstract class Player {
 	/* ====フィールド==== */
-	private static final String[] HANDS = {"グー","チョキ","パー"}; //ジャンケンの手(定数)
-	private int mHand; //ジャンケンの手(プレイヤーの出す手)
+	protected int mHand; //ジャンケンの手(プレイヤーの出す手)
+	
 	/* ====================================================================== */
 	/**
 	 * @brief ジャンケンの手ををあらわす文字列を返却する抽象メソッド
@@ -60,24 +62,56 @@ public abstract class Player {
 	
 	/* ====================================================================== */
 	/**
-	 * @brief ジャンケンの勝敗結果を返却する抽象メソッド
+	 * @brief ジャンケンの勝敗結果を返却するクラスメソッド
 	 *
-	 * @param 
+	 * @param myHand 自分の手　,　enemyHand 相手の手
 	 *
-	 * @return 
+	 * @return 勝敗結果を表す０〜２の整数
 	 *
 	 * @note
+	 * 引数は「０・・グー」、「１・・チョキ」、「２・・パー」を前提とする
 	 * 
+	 * 0・・引き分け（０）
+	 * 1・・負け（１）
+	 * 2・・勝ち（２）
 	 * 
 	 */
 	/* ====================================================================== */
-	public int getResult(Player player){
-		int myHand
+	public static int getResult(int myHand , int enemyHand){
+		//ジャンケンの判定結果を返却
+		return (myHand - enemyHand + 3) % 3;
 	}
 	
 	/* ====================================================================== */
 	/**
-	 * @brief 図形情報の表示と図形の描写を行うメソッド
+	 * @brief ジャンケンの勝敗結果を返却するインスタンスメソッド
+	 *
+	 * @param enemyPlayer 対戦相手のプレイヤー変数
+	 *
+	 * @return 勝敗結果を表す０〜２の整数
+	 *
+	 * @note
+	 * 
+	 */
+	/* ====================================================================== */
+	private int getResult(Player enemyPlayer){
+		int myHand;		//自分のジャンケンの手
+		int enemyHand; //対戦相手のジャンケンの手
+		
+		//自分の手を取得
+		myHand = getHand();
+		//対戦相手の手を決定
+		enemyPlayer.setHand();
+		//対戦相手のジャンケンの手を取得
+		enemyHand = enemyPlayer.getHand(); 
+		
+		//getResultから対戦相手との勝敗結果を取得して返却
+		return getResult(myHand , enemyHand);
+	}
+	
+	/* ====================================================================== */
+	/**
+	 * @brief 
 	 *
 	 * @param enemyPlayer 対戦相手
 	 *
@@ -88,22 +122,35 @@ public abstract class Player {
 	 */
 	/* ====================================================================== */
 	public void play(Player enemyPlayer){
-		int enemyHand; //対戦相手のジャンケンの手
-		int judge; 
-		
-		//対戦相手の手を決定
-		enemyPlayer.setHand();
-		//対戦相手のジャンケンの手を取得
-		enemyHand = enemyPlayer.getHand(); 
+		int judge; //勝敗判定用の変数
 		
 		//setHandメソッドを呼び出して出す手を決定
 		setHand();
+		//対戦相手の手を決定
+		enemyPlayer.setHand();
 		//toStringメソッドを呼び出して出した手の情報の出力
 		System.out.println(toString());
 		//toStringメソッドを呼び出して.対戦相手の出した手の情報の出力
 		System.out.println(enemyPlayer.toString());
 		
-		//getResultメソッドを呼び出して勝敗情報の出力
+		//getResultメソッドを呼び出して勝敗結果を取得
+		judge = getResult(enemyPlayer);
+		
+		//結果が引き分けの場合
+		if(judge == sDRAW){
+			System.out.println("引き分けです。");
+		}
+		
+		//結果が負けの場合
+		if(judge == sLOSE){
+			System.out.println("あなたの負けです。");
+		}
+		
+		//結果が勝ちの場合
+		if(judge == sWIN){
+			System.out.println("あなたの勝ちです。");
+		}
+		
 	}
 
 }
