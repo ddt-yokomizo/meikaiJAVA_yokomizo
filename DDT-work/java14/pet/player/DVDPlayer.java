ca738a3672d6bbac414c332d6f7ed73ca7b00fbd
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-09-13 21:30:25 +0900

    WIP演習１４−３（DVDプレーヤークラス）をコミット（作業中）

---
+++ DDT-work/java14/pet/player/DVDPlayer.java
@@ -0,0 +1,38 @@
+package pet.player;
+
+import model.ExPlayer;
+
+/* ====================================================================== */
+/**
+ * @brief DVDプレイヤークラス
+ *
+ * @note 演習１４−３　DVDの再生、終了、スロー再生を行う
+ */
+/* ====================================================================== */
+public class DVDPlayer implements ExPlayer {
+	
+	/* ====フィールド==== */
+	//なし
+
+	/* ====コンストラクタ==== */
+	//なし
+
+	@Override
+	public void play() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void slowPlay() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	
+}
