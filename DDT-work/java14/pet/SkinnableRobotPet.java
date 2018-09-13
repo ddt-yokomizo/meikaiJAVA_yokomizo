3a8de5b302b5dd20fc96523796ae384a700d2761
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-09-13 20:49:51 +0900

    演習１４−２（着せ替えロボットクラス）をコミット（新規）

--- DDT-work/java14/pet/SkinnableRobotPet.java
+++ DDT-work/java14/pet/SkinnableRobotPet.java
@@ -1,5 +1,9 @@
 package pet;
 
+
+import static number.Constants.INPUT_SKIN_COLOR;
+import static number.Number.inputRangeCheck;
+
 import model.Skinnable;
 
 /* ====================================================================== */
@@ -13,12 +17,95 @@ import model.Skinnable;
 public class SkinnableRobotPet extends RobotPet implements Skinnable {
 
 	/* ====フィールド==== */
-	// なし
+	private int mColor; // スキンの色（）
 
 	/* ====コンストラクタ==== */
-	public SkinnableRobotPet(String name, String masterName) {
+	public SkinnableRobotPet(String name, String masterName , int color) {
 		// スーパークラスのコンストラクタを呼び出す
 		super(name, masterName);
+		//スキンの色を決定
+		setSkin(color);
+	}
+	/* ====================================================================== */
+	/**
+	 * @brief スキンナンバーを返却するメソッド
+	 *
+	 * @param
+	 *
+	 * @return スキンの色　mColor
+	 *
+	 * @note 
+	 * 
+	 * @Override
+	 */
+	/* ====================================================================== */
+	public int getSkinColorNum() {
+		// スキンの色を返却
+		return mColor;
+	}
+	/* ====================================================================== */
+	/**
+	 * @brief スキンナンバーをsetするメソッド
+	 *
+	 * @param　スキンの色　skinColor
+	 *
+	 * @return 
+	 *
+	 * @note 
+	 * 
+	 * @Override
+	 */
+	/* ====================================================================== */
+	public void setSkin(int skinColor) {
+		// スキンの色をset
+		mColor = skinColor;
+	}
+	/* ====================================================================== */
+	/**
+	 * @brief スキンナンバーを返却するメソッド
+	 *
+	 * @param
+	 *
+	 * @return スキンの色　mColor
+	 *
+	 * @note 
+	 * 
+	 * @Override
+	 */
+	/* ====================================================================== */
+	public String getSkinColor() {
+		
+		String skinName = null; //スキンネーム
+		
+		//getSkinColorNumメソッドで取得した整数をキーにスキン名を格納
+		switch (getSkinColorNum()) {
+
+		// 黒の場合
+		case sBLACK:
+			skinName ="黒";
+			break;
+		// 赤の場合
+		case sRED:
+			skinName ="赤";
+			break;
+		// 緑の場合
+		case sGREEN:
+			skinName ="緑";
+			break;
+		// 青の場合
+		case sBLUE:
+			skinName ="青";
+			break;
+		// ヒョウ柄の場合
+		case sLEOPARD:
+			skinName ="ヒョウ柄";
+			break;
+		default:
+			break;
+		}
+		
+		//スキン名を返却
+		return skinName;
 	}
 
 	/* ====================================================================== */
@@ -39,6 +126,8 @@ public class SkinnableRobotPet extends RobotPet implements Skinnable {
 		System.out.println("◇僕は着せ替え可能なロボットであり名前は️" + getName() + "。");
 		// 自己紹介（主人の名前）
 		System.out.println("◇ご主人様は️" + getMasterName() + "。");
+		// 自己紹介（スキン）
+		System.out.println("◇現在のスキンカラーは️" + getSkinColor() + "。");
 	}
 
 	/* ====================================================================== */
@@ -54,37 +143,20 @@ public class SkinnableRobotPet extends RobotPet implements Skinnable {
 	 */
 	/* ====================================================================== */
 	@Override
-	public void changeSkin(int skin) {
-		System.out.print("スキンを");
-		// 引数に応じてスキンを変更してメッセージを表示
-		switch (skin) {
-
-		// 黒の場合
-		case sBLACK:
-			System.out.print("黒");
-			break;
-		// 赤の場合
-		case sRED:
-			System.out.print("赤");
-			break;
-		// 緑の場合
-		case sGREEN:
-			System.out.print("緑");
-			break;
-		// 青の場合
-		case sBLUE:
-			System.out.print("青");
-			break;
-		// ヒョウ柄の場合
-		case sLEOPARD:
-			System.out.print("ヒョウ柄");
-			break;
-		default:
-			break;
-		}
+	public void changeSkin() {
+		int skinColorNum; //スキンカラー番号
 		
+		//スキンの色を入力
+		skinColorNum = inputRangeCheck(sSKIN_MIN, sSKIN_MAX, INPUT_SKIN_COLOR);
+		
+		//スキンを変更
+		setSkin(skinColorNum);
+		
+		System.out.print("スキンを");
+		//getSkinColorメソッドでスキンカラー名を取得して表示
+		System.out.println(getSkinColor());
 		System.out.println("へ変更しました。");
-
+		
 	}
 
 }
