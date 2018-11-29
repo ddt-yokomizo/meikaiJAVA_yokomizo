fb3fad695abe7c951a5100426fd5a296ec0571e1
DDT-yokomizo <ddt.yokomizo@gmail.com>
2018-11-29 20:02:43 +0900

    演習１５−７をコミット(インデント修正)

--- DDT-work/java15/stringBuild/E15_07.java
+++ DDT-work/java15/stringBuild/E15_07.java
@@ -1,21 +1,21 @@
 package stringBuild;
+
 /**
  * 
- * @author masahiro
- * List 15-15 のfor文を拡張for文で実現したプログラムを作成せよ
+ * @author masahiro List 15-15 のfor文を拡張for文で実現したプログラムを作成せよ
  *
  */
 
 public class E15_07 {
 	public static void main(String[] args) {
-		double sum = 0.0; //合計値
-		
-		for(String str : args){
-			//引数の文字列をDouble型に変換して合計する
+		double sum = 0.0; // 合計値
+
+		for (String str : args) {
+			// 引数の文字列をDouble型に変換して合計する
 			sum += Double.parseDouble(str);
 		}
-			System.out.println("合計は" + sum + "です");
-		
+		System.out.println("合計は" + sum + "です");
+
 	}
 
 }
